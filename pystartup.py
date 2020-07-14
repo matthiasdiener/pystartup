ac1e2062e18bfb753a5d24a9fa2dc051abd6ab94
Matthias Diener <mdiener@illinois.edu>
2020-07-14 14:49:20 -0700

    better time

--- pystartup.py
+++ pystartup.py
@@ -3,8 +3,20 @@
 
 from datetime import datetime
 import sys
+
+import sys
+# sys.path.insert(0, 'grudge-2015.1-py3-none-any.whl')
+# sys.path.insert(0, 'dagrt-2019.4-py3-none-any.whl')
+# sys.path.insert(0, 'leap-2019.5-py3-none-any.whl')
+# sys.path.insert(0, 'loo.py-2019.1-py3-none-any.whl')
+# sys.path.insert(0, 'meshmode-2016.1-py3-none-any.whl')
+
+
+import dagrt
 import grudge
-import pyopencl
+import loopy
+import meshmode
+import leap
 
 
 from mpi4py import MPI
@@ -13,6 +25,12 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 if rank == 0:
+    print(dagrt.__file__, file=sys.stderr)
+    print(grudge.__file__, file=sys.stderr)
+    print(loopy.__file__, file=sys.stderr)
+    print(meshmode.__file__, file=sys.stderr)
+    print(leap.__file__, file=sys.stderr)
+
     now = datetime.now()
 
     current_time = now.strftime("%H:%M:%S.%f")
--- pystartup.sbatch.sh
+++ pystartup.sbatch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#SBATCH -N 1
 #SBATCH -t 00:30:00
 #SBATCH -J pystartup
 #SBATCH -p pbatch
@@ -8,24 +7,28 @@ nnodes=$SLURM_JOB_NUM_NODES
 
 echo $nnodes
 
-# Warmup
-srun -n 1 python3 pystartup.py
+export PYOPENCL_NO_CACHE=1
+export LOOPY_NO_CACHE=1
+export XDG_CACHE_HOME=/p/lscratchh/diener3/xdg-scratch
 
-for i in 1 2 4 8 16 32; do
-    for j in $(seq 1 10); do
+# Warmup
+srun -n 1 python3 loopystartup.py
+
+for i in 1 ; do
+    for j in $(seq 1 2); do
         nproc=$((i*nnodes))
         echo $nproc
 
         start=$(date +"%T.%N")
 
-        srun -n $nproc python3 pystartup.py | tee out.txt
+        srun -n $nproc python3 loopystartup.py | tee out$nnodes.txt
 
         end=$(date +"%T.%N")
 
-        mid=$(head -1 out.txt)
+        mid=$(head -1 out$nnodes.txt)
 
-        startup_time=$(ddiff -f '%H:%M:%S.%N' -i '%H:%M:%S.%N' $start $mid)
-        total_time=$(ddiff -f '%H:%M:%S.%N' -i '%H:%M:%S.%N' $start $end)
+        startup_time=$(ddiff -f '%S.%N' -i '%H:%M:%S.%N' $start $mid)
+        total_time=$(ddiff -f '%S.%N' -i '%H:%M:%S.%N' $start $end)
 
         echo $startup_time $total_time >> $nnodes-$i.txt
     done
